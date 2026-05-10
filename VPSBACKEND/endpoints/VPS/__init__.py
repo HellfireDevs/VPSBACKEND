9a628c56cfff245cb843b10a0fc4987638c7d63c
HellfireDevs <devshellfire@gmail.com>
2026-05-10 12:34:28 +0530

    Update __init__.py

--- VPSBACKEND/endpoints/VPS/__init__.py
+++ VPSBACKEND/endpoints/VPS/__init__.py
@@ -1,1 +1,19 @@
+from fastapi import APIRouter
+from .create  import router as create_router
+from .control import router as control_router
+from .delete  import router as delete_router
+from .status  import router as status_router
+from .usage   import router as usage_router
+from .pem     import router as pem_router
+from .ports   import router as ports_router
+from .days    import router as days_router
 
+router = APIRouter()
+router.include_router(create_router)
+router.include_router(control_router)
+router.include_router(delete_router)
+router.include_router(status_router)
+router.include_router(usage_router)
+router.include_router(pem_router)
+router.include_router(ports_router)
+router.include_router(days_router)

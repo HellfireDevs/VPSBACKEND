1d1189e2f97197784b7a125819e8c2d89f906d2d
HellfireDevs <devshellfire@gmail.com>
2026-05-10 12:16:11 +0530

    Update __init__.py

--- VPSBACKEND/endpoints/Plans/__init__.py
+++ VPSBACKEND/endpoints/Plans/__init__.py
@@ -1,1 +1,11 @@
+from fastapi import APIRouter
+from .instances import router as instances_router
+from .ami import router as ami_router
+from .pricing import router as pricing_router
+from .stock import router as stock_router
 
+router = APIRouter()
+router.include_router(instances_router)
+router.include_router(ami_router)
+router.include_router(pricing_router)
+router.include_router(stock_router)
